1656c22e987ce6e20ce5ad809f10e839b193263f
Mikel <rascajones@gmail.com>
2021-03-18 11:57:41 +0100

    Fullscreen for main

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    // Pongo la ventana en pantalla completa
+    w.setWindowState(Qt::WindowFullScreen);
     w.show();
     return a.exec();
 }
